dd11ae637c7beddd0cb1f562e2fd9d85d7344e47
mulni <mulnicrafteroyt@gmail.com>
2021-11-30 22:25:22 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export class Command {
+    name: string = 'help';
+    aliases: string[] = [];
+
+    async init(message: any, client: any, margs: string[]) {
+        if (!margs[0]) {
+            message.channel.send('command will not be run!');
+            return false;
+        }
+
+        return true;
+    }
+
+    async run(
+        message: any,
+        client: any,
+        margs: string[],
+        sargs?: any[],
+        pargs?: any,
+        cargs?: any
+    ) {
+        console.log(cargs);
+        message.channel.send('new cargs added!');
+    }
+
+    async postrun(
+        message: any,
+        client: any,
+        margs: string[],
+        sargs: any,
+        pargs: any
+    ) {
+        message.channel.send('postrun command!');
+    }
+}
